3e7b4f29fe3930c7a1797e2352d208dd63919381
Marcos Dumay de Medeiros <marcos@marcosdumay.com>
2017-02-21 16:26:54 -0300

    Code for issue #1

---
+++ src/Control/Applicative/Acme.hs
@@ -0,0 +1,19 @@
+module Control.Applicative.Acme where
+
+import Control.Applicative
+
+{- |
+Application with the inverse lifting behavior of fmap.
+
+Issue #1
+-}
+pamf :: Applicative f => f (a -> b) -> a -> f b
+pamf f a = f <*> pure a
+
+{- |
+Operator version of pamf
+
+Issue #1
+-}
+(>$<) :: Applicative f => f (a -> b) -> a -> f b
+(>$<) = pamf
